d6af9023e772c84f57e9e57ea1eedf8ce056e608
WengChangWei <13106003@qq.com>
2025-02-13 19:31:46 +0800

    get_user

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() {
     let app = Router::new()
     .route("/", get(root))
     .route("/users", post(create_user))
-    .route("/users/{username}", get(get_user));
+    .route("/users/:username", get(get_user));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
